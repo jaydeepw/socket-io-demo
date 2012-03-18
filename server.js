5b3fe4e92cc6fb0998a5e9e488d293638f50806d
Jaydeep Wagh <jaydeep.w@gmail.com>
2012-03-18 22:58:49 +0530

    socket-io working for now.

---
+++ server.js
@@ -0,0 +1,36 @@
+//var io = require('socket.io').listen(80);
+//
+//io.sockets.on('connection', function (socket) {
+//  socket.emit('news', { hello: 'world' });
+//  socket.on('my other event', function (data) {
+//    console.log(data);
+//  });
+//});
+
+var app = require('http').createServer(handler)
+  , io = require('socket.io').listen(app)
+  , fs = require('fs')
+
+app.listen(80);
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
+
+console.log('Socket server Running now');
